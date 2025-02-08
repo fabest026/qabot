25ea7cbd9aa2750db738e78ddf54893f1bf91aac
Farhan Akbar <161922838+fabest026@users.noreply.github.com>
2025-02-08 12:34:16 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -14,8 +14,7 @@ from typing import List
 genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
 
 # load Gemini Pro model
-model = genai.GenerativeModel("gemini-2.0-flash-thinking-exp-01-21"                    
-  )
+model = genai.GenerativeModel("gemini-2.0-flash" )
 
 # function to get response from Gemini model
 def get_gemini_response(question):
